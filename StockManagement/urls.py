e34e001f795e9522c1196d58071fe1cbbc34f7ef
Dan Bright <productions@aninstance.com>
2018-10-17 20:38:09 +0100

    Mirror push from main development repo

--- StockManagement/urls.py
+++ StockManagement/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, include, static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework')),
     url(r'^api/', include('stock_control.urls', namespace='api')),
     url(r'^', include('stock_control_frontend.urls')),
 ]
